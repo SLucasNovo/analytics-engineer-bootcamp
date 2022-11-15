4ff1704bf0d8ec62e59b7378b1c413a5deaf1827
SlucasNovo <sandrolucasnovo91@gmail.com>
2022-11-15 15:53:43 +0000

    Removed ; issue from supplier_ids

--- analytics_enginner/models/staging/stg_products.sql
+++ analytics_enginner/models/staging/stg_products.sql
@@ -1,1 +1,10 @@
-with source as (select * from {{source('northwind', 'products')}}) Select *, current_timestamp() as ingestion_timestamp from source 
+with source as 
+
+(select 
+    cast(supplier_ids as integer) as supplier_id,
+    * except(supplier_ids) 
+from {{source('northwind', 'products')}} 
+Where supplier_ids not like '%;%'
+) 
+
+Select *, current_timestamp() as ingestion_timestamp from source 
