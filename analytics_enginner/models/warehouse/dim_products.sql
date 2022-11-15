439fe8080622940eb3ea21172705689dd686a503
SlucasNovo <sandrolucasnovo91@gmail.com>
2022-11-15 15:54:28 +0000

    Created dimensional data warehouse layer

---
+++ analytics_enginner/models/warehouse/dim_products.sql
@@ -0,0 +1,26 @@
+
+
+with source as (
+      
+Select 
+ p.id as product_id
+,p.product_code
+,p.product_name
+,p.description
+,s.company as supplier_company
+,p.standard_cost
+,p.list_price
+,p.reorder_level
+,p.target_level
+,p.quantity_per_unit
+,p.discontinued
+,p.minimum_reorder_quantity
+,p.category
+,p.attachments
+ ,current_timestamp() as insertion_timestamp
+        from {{ ref ('stg_products')}} p 
+        left join {{ ref ('stg_suppliers')}} s on s.id = p.supplier_id 
+
+),
+unique_source as ( Select * , row_number() over (partition by product_id) as row_number from source)
+Select * except(row_number) from unique_source where row_number = 1 
